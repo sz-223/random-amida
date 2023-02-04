f07b16924a63c3a98c31af646543bc011cfa63e5
sz-223 <95298879+sz-223@users.noreply.github.com>
2023-02-05 04:17:59 +0900

    modify base

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/random-amida/',
   plugins: [vue()],
   resolve: {
     alias: {
