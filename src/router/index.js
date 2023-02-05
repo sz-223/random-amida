e8cdf917b2a9a39a95ddd3520b4dd2730a3a14d0
sz-223 <95298879+sz-223@users.noreply.github.com>
2023-02-05 15:45:15 +0900

    add workflow_dispatch

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
 import SelectView from '../views/SelectView.vue'
 import GameView from '../views/GameView.vue'
 import ResultView from '../views/ResultView.vue'
@@ -18,7 +19,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: AboutView,
     },
     {
       path: '/settings',
